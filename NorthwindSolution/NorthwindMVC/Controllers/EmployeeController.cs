d478ece8a302323cd6b3ca4367c53ae5ba82ee77
Rifki Ramadhan <riifkiramadhan2@gmail.com>
2024-12-24 17:11:48 +0700

    Kirim latihan project Dot Net MVC sampai ke [Change Main Layout :white_check_mark:]

---
+++ NorthwindSolution/NorthwindMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindMVC.Models;
+
+namespace NorthwindMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        List<Employee> employees = new List<Employee>()
+        {
+            new Employee
+            {
+                Id = 101,
+                Name = "Kang",
+                Salary = 5_000
+            },
+            new Employee
+            {
+                Id = 102,
+                Name = "Widi",
+                Salary = 5_000
+            },
+            new Employee
+            {
+                Id = 103,
+                Name = "Rini",
+                Salary = 5_000
+            }
+        };
+
+        // GET: EmployeeController
+        public ActionResult Index()
+        {
+            return View(employees);
+        }
+
+        // GET: EmployeeController/Details/5
+        public ActionResult Details(int id)
+        {
+            var employee = employees.FirstOrDefault(x => x.Id.Equals(id));
+            
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return View(employee);
+        }
+
+        // GET: EmployeeController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: EmployeeController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: EmployeeController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ NorthwindSolution/NorthwindMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using NorthwindMVC.Models;
+
+namespace NorthwindMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["bootcamp"] = "Bootcamp .NET";
+            ViewData["waktu"] = DateTime.Now;
+            ViewData["price"] = 500_000;
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
